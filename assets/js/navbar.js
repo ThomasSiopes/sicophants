60cf44717fefa14f40037408946df78d5f32c01f
theeshyguy <t.siopes00@yahoo.com>
2021-05-24 15:58:11 -0400

    removed unnecessary files and added new ones

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -6,7 +6,7 @@ function generateNavbar() {
     newNav.classList.add("navbar-expand-md");
     newNav.classList.add("navbar-dark");
     newNav.classList.add("bg-red");
-    newNav.innerHTML = '<a class="navbar-brand mx-2" href="index.html" title="Home">Site Name</a>\
+    newNav.innerHTML = '<a class="navbar-brand mx-2" href="./index.html" title="Home">Site Name</a>\
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
       <span class="navbar-toggler-icon"></span>\
     </button>\
---
+++ assets/js/topic_nav.js
@@ -0,0 +1,37 @@
+const allData = global_database;
+let topicList = document.querySelector("#myUL");
+
+function fillAuthorList() {
+    topicArray = topic_compiler();
+
+    let newTopicName;
+    let newLink;
+    for(let i = 0; i < topicArray.length; ++i) {
+        newTopicName = document.createElement("li");
+        newLink = document.createElement("a");
+
+        newLink.textContent = topicArray[i];
+        newLink.href="./topic-pages/topic" + (topicArray[i].replace(/\s/g, '')) + ".html";
+
+        newTopicName.appendChild(newLink);
+        topicList.appendChild(newTopicName);
+    }
+}
+
+function topic_compiler() {
+    var topicARR = [];
+    for(var i = 0; i < allData.length; ++i) {
+        for(var j = 0; j < allData[i].quotes.length; ++j) {
+            let inChecker = 0;
+            for(var k = 0; k < allData[i].quotes[j].topics.length; ++ k) {
+                for(var l = 0; l < topicARR.length; ++l) {
+                    if(topicARR[l] == allData[i].quotes[j].topics[k]) inChecker = 1;
+                }
+                if(inChecker === 0) topicARR.push(allData[i].quotes[j].topics[k]);
+            }
+        }
+    }
+    return topicARR;
+}
+
+fillAuthorList();
