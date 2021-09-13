8b12eb129abf6164cae1af3e77c238fd66d7899a
theeshyguy <t.siopes00@yahoo.com>
2021-09-13 15:06:51 -0400

    renamed db

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # To Do
 - Find something to fill up white space on non-scrolling pages
 - Fill out rest of quote pile up
+- Fix mobile share button
 
 # WIP
 - Fix Heroku deployment
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/sicophants',
+    process.env.MONGODB_URI || 'mongodb://localhost/proverbial',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
