60e799f3b011edd886a4cba3988f38e2a22598f8
theeshyguy <t.siopes00@yahoo.com>
2021-12-08 11:12:11 -0500

    fixed connection error

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    // process.env.MONGODB_URI || 'mongodb://localhost/proverbial',
-    "mongodb+srv://tsiopes00:Sepois00@cluster0.supq7.mongodb.net/proverbial?retryWrites=true&w=majority",
+    process.env.MONGODB_URI || 'mongodb://localhost/proverbial',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
